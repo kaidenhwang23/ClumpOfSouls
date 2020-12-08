30fa11819e4197ba12e23e2f259cab584d90e7b4
kaidenhwang23 <kaidenhwang@gmail.com>
2020-12-08 11:20:36 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,102 @@
+//python -m SimpleHTTPServer 8000
+
 let player;
+let start = false;
 
 function setup(){
   createCanvas(800, 800);
-  let player = new Player(20,20,50,50)
+  player = new Player(400,400,25,0,0)
+  rectMode(CENTER);
 
 }
 
 function draw(){
 	background(220);
-  player.drawPlayer();
+  if(start == false){
+    fill("green")
+    rect(400,400,350,100)
+    stroke("black")
+    fill("black")
+    textSize(30)
+    text("press space to start", 275,405)
+  }else{
+    null
+  }
+
+
+  if(start == true){
+    player.drawPlayer();
+    player.boundaries();
+}
 }
 
 
 function keyPressed(){
-  if(keyCode == UP_ARROW){
-    this.ydirection * -1
-    this.xdirection = 0
+  if(start == true){
+    if(keyCode == UP_ARROW){
+      player.yspeed = -3
+      player.xspeed = 0
+    }
+    if(keyCode == DOWN_ARROW){
+      player.yspeed = 3
+      player.xspeed = 0
+    }
+    if(keyCode == RIGHT_ARROW){
+      player.xspeed = 3
+      player.yspeed = 0
+    }
+    if(keyCode == LEFT_ARROW){
+      player.xspeed = -3
+      player.yspeed = 0
+    }
   }
-  if(keyCode == DOWN_ARROW){
-    this.ydirection * 1
-    this.xdirection = 0
-  }
-  if(keyCode == RIGHT_ARROW){
-    this.xdirection * 1
-    this.ydirection = 0
-  }
-  if(keyCode == LEFT_ARROW){
-    this.xdirection * -1
-    this.ydirection = 0
+  if(keyCode == 32){
+    start = true
   }
 }
 
 
+
+
+
 class Player {
-  constructer(x,y,width,height){
-    this.width = width
-    this.height = height
-    this.xdirection = 10
-    this.ydirection = 10
+  constructor(x,y,size,xspeed,yspeed){
+    this.size = size
+    this.xspeed = xspeed
+    this.yspeed = yspeed
     this.x = x
     this.y = y
   }
 
   drawPlayer(){
     fill("black")
-    ellipse(this.x, this.y, this.width, this.height)
-    console.log(this.x, this.y)
+    ellipse(this.x+=this.xspeed, this.y+=this.yspeed, this.size)
+  }
+
+  boundaries(){
+    if(this.x > 810){
+      this.x = -10
+    }
+    if(this.x < -10){
+      this.x = 810
+    }
+    if(this.y < -10){
+      this.y = 810
+    }
+    if(this.y > 810){
+      this.y = -10
+    }
   }
 }
+
+  class Food {
+    constructor(x,y,size){
+      this.x = x
+      this.y = y
+      this.size = size
+    }
+
+    drawFood(){
+
+    }
+  }
